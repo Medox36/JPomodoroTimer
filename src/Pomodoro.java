280f352aa07a825e4d4567a572090a35163c7634
Medox36 <giuntinil@bzz.ch>
2021-10-11 20:26:30 +0200

    reset the timer logic

---
+++ src/Pomodoro.java
@@ -0,0 +1,14 @@
+import java.time.format.DateTimeFormatter;
+
+public class Pomodoro {
+    private DateTimeFormatter timeFormatter;
+
+    public Pomodoro() {
+        timeFormatter = DateTimeFormatter.ofPattern("mm:ss");
+    }
+
+    public void startRedTime() {
+
+    }
+
+}
