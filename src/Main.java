5f437259319ef619df6925f72678ec8e85bd0ce4
Medox36 <giuntinil@bzz.ch>
2021-10-13 14:34:49 +0200

    started declaring methods of Settings.java

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,6 @@ public class Main {
 
     public void run() {
         PomodoroGUI pomodoroGUI = new PomodoroGUI();
+        Settings settings = new Settings();
     }
 }
--- src/Settings.java
+++ src/Settings.java
@@ -1,3 +1,25 @@
-public class Settings {
+import java.io.File;
 
+public class Settings {
+    private File standardSettingsFile;
+
+    public Settings() {
+        standardSettingsFile = new File("settings/settings.txt");
+    }
+
+    private void loadContentsFromFile() {
+
+    }
+
+    private void loadContentsFromFile(File file) {
+
+    }
+
+    private void saveContentsToFile() {
+
+    }
+
+    private void saveContentsToFile(File file) {
+
+    }
 }
