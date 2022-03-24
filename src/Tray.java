252b7c3fedf988bb30fa07bc469bb2505d9048b7
Medox36 <giuntinil@bzz.ch>
2022-03-24 18:18:12 +0100

    removed unnecessary attribute settings:Settings in Tray.java

--- src/Tray.java
+++ src/Tray.java
@@ -4,12 +4,10 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class Tray extends TrayIcon {
-    private Settings settings;
     private final PomodoroGUI pomodoroGUI;
 
     public Tray(PopUpMenu popUpMenu, PomodoroGUI pomodoroGUI) {
         super(Images.frameIcon.getImage(), "Pomodoro Timer", popUpMenu);
-        settings = Settings.getInstance();
         this.pomodoroGUI = pomodoroGUI;
         setImageAutoSize(true);
         addMouseListener(new MouseAdapter() {
